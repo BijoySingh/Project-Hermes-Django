4baf854fbfa371f8b11862ba94a7df2e17459089
bijoy <bijoysingh693@gmail.com>
2016-04-07 18:08:17 +0530

    Removing Python2.7 project

--- account/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class UserProfile(models.Model):
-    user = models.ForeignKey(User)
-    reputation = models.FloatField(default=0)
